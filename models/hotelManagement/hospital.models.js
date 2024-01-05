38c446b5db31811e303ecc00e248f365034c4ef0
Baibhav <baibhavtiwari37@gmail.com>
2024-01-05 22:19:13 +0530

    Hospital ✅

---
+++ models/hotelManagement/hospital.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  addressLine1: {
+    type: String,
+    required: true,
+  },
+  addressLine2: {
+    type: String,
+  },
+  pincode: {
+    type: String,
+    required: true,
+  },
+  Specialization: [
+    {
+      type: String,
+    }],
+}, { timestamps: true });
+
+export const Hospital = mongoose.Model("Hospital", hospitalSchema);
